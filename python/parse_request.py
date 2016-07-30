15537ae7bd28568da5b06e54f542289705acd625
cenaka <cenaka@yandex.ru>
2016-07-31 02:55:48 +0300

    lemmatization categories added

--- python/parse_request.py
+++ python/parse_request.py
@@ -1,11 +1,12 @@
 import logging
 import sys
+
+from pymystem3 import Mystem
+
 from download_media import download_and_cut_song
 
 logging.basicConfig(level=logging.DEBUG, stream=sys.stderr)
 import csv
-import json
-import os
 import requests
 import time
 from common import Artist, Track, get_conn, normalize_name
@@ -20,6 +21,8 @@ FROM_VALUES_API = API_ROOT + "stream_from_values.api"
 SIMILAR_PERFORMANCES_API = API_ROOT + "similar_performers.api"
 SONGS_BY_PERFORMER = API_ROOT + "get_songs_by_performer.api"
 
+M = Mystem()
+
 artists_cache = {
 
 }
@@ -27,8 +30,13 @@ tracks_cache = {
 
 }
 
+
 def file_to_dict(file):
-    return {row[1].lower(): row[0] for row in csv.reader(open(file))}
+    type_to_id = {normalize_name(row[1]): row[0] for row in csv.reader(open(file))}
+    normalize = {''.join(M.lemmatize(key)).strip():type_to_id[key] for key in type_to_id}
+    type_to_id.update(normalize)
+    return type_to_id
+
 
 def get_categories():
     categ_dict = {}
@@ -36,14 +44,16 @@ def get_categories():
         categ_dict.update(file_to_dict(file))
     return categ_dict
 
+
 def get_indexes(input_str, categ_dict):
-    categories = [i.strip() for i in input_str.split(',')]
+    categories = [normalize_name(i) for i in input_str.split(',')]
     indxs = []
     for category in categories:
-        ind = categ_dict.get(category.lower())
+        ind = categ_dict.get(normalize_name(category))
         if ind:
             indxs.append(ind)
     return indxs
+
 
 def get_muzis_songs(indxs):
     values = ','.join(['{}:100'.format(i) for i in indxs])
@@ -74,9 +84,12 @@ def generate_normalized_aliases(name, aliases, type):
 
 
 def get_last_fm_api_response(name):
-    r = requests.get(LAST_FM, params=dict(method="artist.gettoptracks", artist=name, api_key="eb7e02525703c69c761d0a17ca11923c", format="json"))
+    r = requests.get(LAST_FM,
+                     params=dict(method="artist.gettoptracks", artist=name, api_key="eb7e02525703c69c761d0a17ca11923c",
+                                 format="json"))
     best_tracks = [t["name"] for t in r.json()["toptracks"]["track"][:3]]
     return best_tracks
+
 
 def make_artist(id, name, photo, related_names):
     logging.debug("last.fm api: {}:{}".format(id, name))
@@ -97,7 +110,7 @@ def get_similar_artists(artist_id):
     r = requests.post(SONGS_BY_PERFORMER,
                       data={"performer_id": artist_id,
                             'size': SIMILAR_ARTISTS_DEFAULT_SIZE}
-    )
+                      )
     logging.debug("Done")
     songs = r.json()["songs"]
     song_with_artist_info = None
@@ -107,7 +120,8 @@ def get_similar_artists(artist_id):
             break
     if not song_with_artist_info:
         logging.error("PERFORMER NOT FOUND FOR {}".format(artist_id))
-    return make_artist(song_with_artist_info['performer_id'], song_with_artist_info['performer'], song_with_artist_info['poster'], [])  # TODO similar
+    return make_artist(song_with_artist_info['performer_id'], song_with_artist_info['performer'],
+                       song_with_artist_info['poster'], [])  # TODO similar
 
 
 def make_track_name_aliases(param):
@@ -137,6 +151,7 @@ def try_get_artists_from_db(artists):
         remains = [art for art in remains if not artists_cache.get(art)]
     return result, remains
 
+
 def try_get_song_from_db(song_id):
     if song_id in tracks_cache:
         return tracks_cache[song_id]
@@ -145,7 +160,7 @@ def try_get_song_from_db(song_id):
     """, [song_id])
     row = result_set.fetchone()
     if row:
-        json_obj,  = row
+        json_obj, = row
         track = Track.from_json(json_obj)
         artists, remains = try_get_artists_from_db(track.artists)
         if remains:
@@ -172,11 +187,13 @@ def make_track_from_song(song):
         if remains:
             remains_ = [get_similar_artists(artist_id) for artist_id in remains]
             real_artists.extend(remains_)
-        new_track = Track(song_id, real_artists, song["track_name"], make_track_name_aliases(song["track_name"]), song["file_mp3"])
+        new_track = Track(song_id, real_artists, song["track_name"], make_track_name_aliases(song["track_name"]),
+                          song["file_mp3"])
         download_and_cut_song(song["file_mp3"])
         logging.debug("Track Done")
         put_track_to_db(new_track)
         return new_track
+
 
 def put_artists_to_db(artists):
     artists = [artist for artist in artists if artist.id not in artists_cache]
@@ -189,6 +206,7 @@ def put_artists_to_db(artists):
     for artist in artists:
         artists_cache[artist.id] = artist
 
+
 def put_track_to_db(track):
     put_artists_to_db(track.artists)
     track.artists = [a.id for a in track.artists]
@@ -200,11 +218,13 @@ def put_track_to_db(track):
     conn.commit()
     tracks_cache[track.id] = track
 
+
 def main():
     # global input_str, file_to_dict, categ_dict, indxs, songs, result, song, track, i
-    input_str = "Русский"# input()
+    input_str = "Русский, пляжная"  # input()
     categ_dict = get_categories()
     indxs = get_indexes(input_str, categ_dict)
+    print(indxs)
     songs = get_muzis_songs(indxs)
     result = []
     logging.debug("Loaded songs: {}".format([song['id'] for song in songs]))
