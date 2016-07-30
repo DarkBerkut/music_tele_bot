19df06ca77c9e274eab35d5fb08fffefffe086df
cenaka <cenaka@yandex.ru>
2016-07-30 20:59:39 +0300

    paths to downloading changed

--- python/music_cut.py
+++ python/music_cut.py
@@ -4,9 +4,9 @@ from pydub import AudioSegment
 TIME_IN_SECONDS = 15 * 1000
 song_name = 'wuyexyc1lzmo'
 r = requests.get("http://f.muzis.ru/{}.mp3".format(song_name))
-with open("data/music/{}.mp3".format(song_name), 'wb') as file:
+with open("data/music/full/{}.mp3".format(song_name), 'wb') as file:
     file.write(r.content)
 
-song = AudioSegment.from_mp3("data/music/{}.mp3".format(song_name))
+song = AudioSegment.from_mp3("data/music/full/{}.mp3".format(song_name))
 cut_song = song[:TIME_IN_SECONDS]
-cut_song.export("data/music/cut_{}.mp3".format(song_name), format='mp3')
+cut_song.export("data/music/cut/cut_{}.mp3".format(song_name), format='mp3')
