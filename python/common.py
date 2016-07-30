c120aa4943ee6d7e1559a18ace01f9be0c2c1f72
tema-popov <artpop92@gmail.com>
2016-07-30 19:40:00 +0300

    reqs

--- python/common.py
+++ python/common.py
@@ -1,5 +1,7 @@
 __author__ = 'jambo'
 
-
 def normalize_name(string):
     return " ".join("".join([ch if ch.isalpha() or ch.isdigit() else " " for ch in string.lower()]).split())
+
+from Levenshtein import distance as lev_distance
+
